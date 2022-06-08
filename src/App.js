dc196eb9b4f03c896502ee6dd3899968d56018f3
Hosea-Kibet <kibethosea6@gmail.com>
2022-06-08 08:42:57 +0300

    updated header calender customers,Editor,Employees,Kanban,Orders

--- src/App.js
+++ src/App.js
@@ -74,7 +74,7 @@ const App = () => {
             <Routes>
               {/* Dashboard */}
               <Route path="/" element={<Ecommerce />} />
-              <Route path="/eccomerce" element={<Ecommerce />} />
+              <Route path="/ecommerce" element={<Ecommerce />} />
               {/* Pages */}
               <Route path="/orders" element={<Orders />} />
               <Route path="/employees" element={<Employees />} />
@@ -82,7 +82,7 @@ const App = () => {
               {/* Apps */}
               <Route path="/kanban" element={<Kanban />} />
               <Route path="/editor" element={<Editor />} />
-              <Route path="/calender" element={<Calendar />} />
+              <Route path="/calendar" element={<Calendar />} />
               <Route path="/color-picker" element={<ColorPicker />} />
               {/* Charts */}
               <Route path="/line" element={<Line />} />
